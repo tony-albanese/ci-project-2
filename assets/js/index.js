6d730dea7050ba94ff5bd545c1ba3a1ca245890b
Tony Albanese <tony.albanese2000@gmail.com>
2022-12-04 18:31:19 +0100

    Open game in same window

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
     let gameButton = document.getElementById("open-game-button");
 
     gameButton.addEventListener("click", function () {
-            window.open("game.html").focus();
+            window.open("game.html", "_self").focus();
     });
 });
