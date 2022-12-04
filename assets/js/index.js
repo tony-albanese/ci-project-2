b3e72253391711f1a9bd8bcc2a9c95cac7b55fac
tony-albanese <38885564+tony-albanese@users.noreply.github.com>
2022-12-04 18:53:24 +0100

    Landing page (#10)
    * initial commit
    
    * added citation for hero image
    
    * add background image
    
    * initial commit
    
    * add h1 and p elements to hero area
    
    * decrease height of hero section
    
    * added rules to section
    
    * initial commit
    
    * added citations for favicon images
    
    * add favicon link
    
    * add some text to the hero section
    
    * initial commit
    
    * style overlay for hero and rules section
    
    * enclose hero and rules content in divs
    for overlay styling
    
    * add play button
    
    * styled contents of hero overlay
    
    * adjust height of overlay
    increased font size of p elements
    
    * surrounded game text with p tags
    
    * styled content in rules section
    
    * add footer element
    
    * style list items for extra spacing
    
    * styled background and padding for footer
    
    * add image credit to footer
    
    * refactored landing.page to index
    
    * refactored original index to game.html
    
    * add listener to button to open game page
    
    * centered and fixed button color
    
    * set darker green for button color
    
    * add and style image credits to side menu
    
    * fixed trailing slash
    
    * add section about landing page.
    
    * add section on version control and testing
    
    * Add section on testing using console
    
    * Open game in same window
    
    * add media queries to landing page

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    let gameButton = document.getElementById("open-game-button");
+
+    gameButton.addEventListener("click", function () {
+            window.open("game.html", "_self").focus();
+    });
+});
