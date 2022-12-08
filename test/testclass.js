3a4091ecc969dce23d801e4e5d23ded3656fa529
Tony Albanese <tony.albanese2000@gmail.com>
2022-12-08 09:32:20 +0100

    format and add comments

--- test/testclass.js
+++ test/testclass.js
@@ -1,50 +1,55 @@
+/**
+ * This class contains a copy of the method used to return a hashmap off all the possible responses based on the 
+ * the possible combinations of keys. 
+ */
 class TestMethods {
-  
 
     static createResponseMap() {
         let responseMap = new Map();
-    
+
         responseMap.set('scissorspaper', 'Scissors cuts paper.');
         responseMap.set('paperscissors', 'Scissors cuts paper.');
-    
+
         responseMap.set('paperrock', 'Paper covers rock.');
         responseMap.set('rockpaper', 'Paper covers rock.');
-    
+
         responseMap.set('rocklizard', 'Rock crushes lizard.');
         responseMap.set('lizardrock', 'Rock crushes lizard.');
-    
+
         responseMap.set('lizardspock', 'Lizard poisons Spock.');
         responseMap.set('spocklizard', 'Lizard poisons Spock.');
-    
+
         responseMap.set('spockscissors', 'Spock smashes scissors');
         responseMap.set('scissorsspock', 'Spock smashes scissors');
-    
+
         responseMap.set('scissorslizard', 'Scissors decapitates lizard.');
         responseMap.set('lizardscissors', 'Scissors decapitates lizard.');
-    
+
         responseMap.set('lizardpaper', 'Lizard eats paper.');
         responseMap.set('paperlizard', 'Lizard eats paper.');
-    
+
         responseMap.set('paperspock', 'Paper disporoves spock');
         responseMap.set('spockpaper', 'Paper disporoves spock');
-    
+
         responseMap.set('spockrock', 'Spock vaporizes rock.');
         responseMap.set('rockspock', 'Spock vaporizes rock.');
-    
+
         responseMap.set('rockscissors', 'Rock crushes scissors.');
         responseMap.set('scissorsrock', 'Rock crushes scissors.');
-    
+
         return responseMap;
     }
-    
-    
+
+
 }
 
 module.exports = TestMethods;
 
-
-
-function testMap(){
+/**
+ * This method ensures that the map returns the correct string for every key by printing the string
+ * to the console.
+ */
+function testMap() {
     let map = TestMethods.createResponseMap();
     console.log(map.get("scissorspaper"));
     console.log(map.get("paperscissors"));
